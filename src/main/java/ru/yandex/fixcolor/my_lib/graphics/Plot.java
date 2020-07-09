ef051e8585c8cce0fd4ea1c4d5b21c597d2a7ded
PolBadWolf <gm.gameoff@gmail.com>
2020-07-09 18:14:21 +0500

    add Plot

---
+++ src/main/java/ru/yandex/fixcolor/my_lib/graphics/Plot.java
@@ -0,0 +1,11 @@
+package ru.yandex.fixcolor.my_lib.graphics;
+
+import javafx.scene.canvas.Canvas;
+
+public class Plot {
+    private Canvas  canvas = null;
+
+    public Plot(Canvas canvas) {
+        this.canvas = canvas;
+    }
+}
